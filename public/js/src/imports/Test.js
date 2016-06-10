68a9f4dc1918cf8e5c05a1a10fce1ab845772106
Sagi Nadir <saginadir@gmail.com>
2016-06-11 02:19:07 +0300

    Test class changed a bit to show more functionality

--- public/js/src/imports/Test.js
+++ public/js/src/imports/Test.js
@@ -1,9 +1,29 @@
 "use strict";
 
+import $ from "../../vendor/jquery/dist/jquery.min"
+import _ from "../../vendor/lodash/dist/lodash.min"
+
 class Test {
     constructor() {
+        let test = [
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 4",  
+        ];
+
+        $("body").append("Hello World.");
+
+        let newList = "<ul>";
+        _.each(test,(value) => {
+            newList += `<li>${value}</li>`;
+        });
+        newList += "</ul>";
+
+        $("body").append(newList);
+
         console.log("Hello World.");
     }
 }
- 
+
 export default Test;
