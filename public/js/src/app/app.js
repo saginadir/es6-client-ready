2dd68f88dbafa1ecd78b76547aceac8583ff2b6f
Sagi Nadir <saginadir@gmail.com>
2016-06-11 01:48:09 +0300

    app.js and Test.js import committed

---
+++ public/js/src/app/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import Test from "../imports/Test";
+
+let test = new Test();
---
+++ public/js/src/imports/Test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class Test {
+    constructor() {
+        console.log("Hello World.");
+    }
+}
+ 
+export default Test;
