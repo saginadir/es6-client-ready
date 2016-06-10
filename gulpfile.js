d9c1dcbb6476c8cf6935f6e61d13f8d106c5b853
Sagi Nadir <saginadir@gmail.com>
2016-06-11 01:59:34 +0300

    gulp file updated to support bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,16 @@ const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const gutil = require('gulp-util');
+const mainBowerFiles = require('main-bower-files');
 // const minify = require('gulp-minify');
+
+// This is a task just to place bower packages in the correct place
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles(), {
+        base: 'bower_components'
+    })
+        .pipe(gulp.dest('public/js/vendor'));
+});
 
 // Lets bring es6 to es5 with this.
 // Babel - converts ES6 code to ES5 - however it doesn't handle imports.
